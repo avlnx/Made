11cf5a4a8960dfce102bb200e8c5c0a15a485369
Thyago F. da Silva <thferreira@gmail.com>
2018-02-23 22:55:05 -0300

    Installs and sets up basic react-native-navigation

--- App.js
+++ App.js
@@ -1,32 +1,93 @@
 import React from 'react';
-import {Text, View} from 'react-native';
-import {NativeRouter, Link, Router} from 'react-router-native';
-import {Navigation, Card, Tabs, Tab} from 'react-router-navigation';
+import {Text, View, StyleSheet, Button} from 'react-native';
+import {StackNavigator} from 'react-navigation';
 
-import {Tabber} from './src/components/screens/Tabber';
+class SplashScreen extends React.Component {
 
-const App = () => (
-    <NativeRouter>
-      <Navigation title={'Hermes'}>
-        <Card
-            exact
-            path="/"
-            render={() => (
-                <View>
-                  <Text>Home Screen</Text>
-                  <Link to="/login">
-                    <Text>Login</Text>
-                  </Link>
-                  <Link to="/tabs">
-                    <Text>Tabs</Text>
-                  </Link>
-                </View>
-            )}
-        />
-        <Card path="/login" render={() => <Text>Login Screen</Text>}/>
-        <Card path='/tabs' render={Tabber} />
-      </Navigation>
-    </NativeRouter>
+  render() {
+    return (
+        <View style={styles.screen}>
+          <Text style={styles.screenText}>Splash Screen</Text>
+          <Button
+              onPress={() => this.props.navigation.navigate('Login')}
+              title="Login"
+          />
+        </View>
+    );
+  }
+
+  /* render function, etc */
+}
+
+class LoginScreen extends React.Component {
+  render() {
+    return (
+        <View style={styles.screen}>
+          <Text style={styles.screenText}>LoginScreen</Text>
+          <Button
+              onPress={() => this.props.navigation.navigate('LoggedInStack')}
+              title="Login"
+          />
+        </View>
+    );
+  }
+}
+
+class OnboardingScreen extends React.Component {
+  render() {
+    return (
+        <View style={styles.screen}>
+          <Text style={styles.screenText}>Onboarding Screen</Text>
+          <Button
+              onPress={() => this.props.navigation.navigate('LoggedOutStack')}
+              title="Sign Out"
+          />
+        </View>
+    );
+  }
+}
+
+const LoggedOutStack = StackNavigator(
+    {
+      Splash: {
+        screen: SplashScreen,
+      },
+      Login: {
+        screen: LoginScreen,
+      },
+    },
+    {
+      headerMode: 'none',
+    },
 );
 
-export default App;
+const RootStack = StackNavigator(
+    {
+      LoggedOutStack: {
+        screen: LoggedOutStack,
+      },
+      LoggedInStack: {
+        screen: OnboardingScreen,
+      },
+    },
+    {
+      headerMode: 'none',
+    }
+);
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  screenText: {
+    fontSize: 42,
+  },
+});
+
+export default class App extends React.Component {
+  render() {
+    return <RootStack />;
+  }
+}
