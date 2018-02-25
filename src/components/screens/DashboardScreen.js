136926ff6d67f1cd4ef18ee9f1a9e7b582078ccb
Thyago F. da Silva <thferreira@gmail.com>
2018-02-24 22:18:25 -0300

    Fixes sign-out button in dashboard

--- src/components/screens/DashboardScreen.js
+++ src/components/screens/DashboardScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Container, Content, H1, H2, Button, Text} from 'native-base';
+import {Container, Content, H1, H2, Button, Text, View} from 'native-base';
 // import {getResetAndNavigateActionTo} from '../../navigators';
 
 class DashboardScreen extends React.Component {
@@ -7,22 +7,23 @@ class DashboardScreen extends React.Component {
     title: 'Dashboard',
   };
 
-  // resetAndNavigate(routeName) {
-  //   const action = getResetAndNavigateActionTo(routeName);
-  //   this.props.navigation.dispatch(action);
-  // }
+  resetAndNavigate(routeName) {
+    const action = getResetAndNavigateActionTo(routeName);
+    this.props.navigation.dispatch(action);
+  }
 
   render() {
     return (
         <Container>
-          <Content padder>
+          <View style={{padding: 10}}>
             <H2>Seus pontos de venda</H2>
             <Button
+                transparent
                 onPress={() => this.resetAndNavigate('LoggedOutStack')}
             >
               <Text>Sign Out</Text>
             </Button>
-          </Content>
+          </View>
         </Container>
     );
   }
