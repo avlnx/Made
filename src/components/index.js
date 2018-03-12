c57f1b128f790d02ca15fffa20366b5399df6a7f
Thyago F. da Silva <thferreira@gmail.com>
2018-03-11 21:55:26 -0300

    Refactor StoreProductList into a component

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export * from './LoginForm';
 export * from './LoadPosForm';
 export * from './StoreList';
+export * from './StoreProductList';
--- src/screens/StoreFrontScreen.js
+++ src/screens/StoreFrontScreen.js
@@ -1,36 +1,16 @@
 import React from 'react';
-import {Image, FlatList, Dimensions, StyleSheet} from 'react-native';
 import {
   Container,
   Content,
-  View,
-  Text,
-  List,
-  ListItem,
-  Card,
-  CardItem,
-  H1,
-  Left,
-  Body,
-  Right,
-  H2,
-  H3,
-  Button,
-  Icon
 } from 'native-base';
-import {Col, Row, Grid} from 'react-native-easy-grid';
-import {getResetAndNavigateActionTo} from '../navigators/index';
 import {connect} from 'react-redux';
 import actions from '../reducers/actions';
 import firebase from 'react-native-firebase';
 import {MadeLogo} from '../components/common';
+import {StoreProductList} from '../components';
 
 class StoreFrontScreen extends React.Component {
 
-  // resetAndNavigate(routeName) {
-  //   const action = getResetAndNavigateActionTo(routeName);
-  //   this.props.navigation.dispatch(action);
-  // }
   constructor() {
     super();
     this.db = firebase.firestore();
@@ -79,62 +59,11 @@ class StoreFrontScreen extends React.Component {
   }
 
   render() {
-    const {activeStore, productListInStock} = this.props;
-    const numColumns = 2;
-    const itemMargin = 15;
-    const size = (Dimensions.get('window').width - (itemMargin * 4)) / numColumns;
-    const styles = StyleSheet.create({
-      itemContainer: {
-        width: size,
-        height: 400,
-        margin: itemMargin,
-      },
-      item: {
-        flex: 1,
-      },
-    });
     return (
         <Container>
           <Content>
             <MadeLogo/>
-            <FlatList
-                data={productListInStock}
-                numColumns={numColumns}
-                keyExtractor={item => item.id}
-                renderItem={({item}) =>
-                    <View style={styles.itemContainer}>
-                      <Card style={styles.item}>
-                        <CardItem cardBody>
-                          <Image source={{uri: item.image}}
-                                 style={{height: 250, width: null, flex: 1}}/>
-                        </CardItem>
-                        <CardItem>
-                          <Body style={{flex: 2}}>
-                          <H3>{item.title}</H3>
-                          {/*<Text note>{item.description}</Text>*/}
-                          </Body>
-                          <Right style={{flex: 1}}>
-                            <Text style={{
-                              fontSize: 24,
-                              fontWeight: 'bold',
-                            }}>R$ {item.publicPrice}</Text>
-                          </Right>
-                        </CardItem>
-                        <CardItem style={{justifyContent: 'flex-end'}}>
-                          <Button transparent small>
-                            <Text>Mais informações</Text>
-                          </Button>
-                          <Button style={{marginRight: 10}}>
-                            <Icon name='remove'/>
-                          </Button>
-                          <Button>
-                            <Icon name='add'/>
-                          </Button>
-                        </CardItem>
-                      </Card>
-                    </View>
-                }>
-            </FlatList>
+            <StoreProductList/>
           </Content>
         </Container>
     );
