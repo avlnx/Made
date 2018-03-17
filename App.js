bac0ce31aacc98243e70a8c95ad1270a2b9cda9f
Thyago F. da Silva <thferreira@gmail.com>
2018-03-17 18:41:02 -0300

    Refactors App.js and redux

--- App.js
+++ App.js
@@ -1,25 +1,13 @@
 import React from 'react';
+// Theming
 import {StyleProvider} from 'native-base';
 import getTheme from './native-base-theme/components';
 import commonColor from './native-base-theme/variables/commonColor';
-import {createStore, applyMiddleware, combineReducers} from 'redux';
+// Redux
 import {Provider} from 'react-redux';
-import thunkMiddleware from 'redux-thunk';
-import {createLogger} from 'redux-logger';
-
-import * as reducers from './src/reducers';
-
+import store from './src/reducers';
+// Navigators
 import {RootStack} from './src/navigators';
-
-const reducer = combineReducers(reducers);
-
-const loggerMiddleware = createLogger();
-
-const store = createStore(reducer,
-    applyMiddleware(
-        thunkMiddleware,
-        loggerMiddleware,
-    ));
 
 export default class App extends React.Component {
   render() {
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,21 @@
 import auth from './auth';
 import ui from './ui';
 import stores from './stores';
+import thunkMiddleware from 'redux-thunk/index';
+import {applyMiddleware, combineReducers, createStore} from 'redux/index';
+import {createLogger} from 'redux-logger';
 
-export {
+const reducers = {
   auth,
   ui,
   stores,
 };
+
+const reducer = combineReducers(reducers);
+const loggerMiddleware = createLogger();
+
+export default store = createStore(reducer,
+    applyMiddleware(
+        thunkMiddleware,
+        loggerMiddleware,
+    ));
