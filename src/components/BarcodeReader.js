93feb6d5655c6d237d54514f28f714fe8a296791
Thyago F. da Silva <thferreira@gmail.com>
2018-03-25 23:15:44 -0300

    Bug in react-native-camera issue #1234

--- src/components/BarcodeReader.js
+++ src/components/BarcodeReader.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import {View,Text} from 'native-base';
 import { RNCamera } from 'react-native-camera';
+import {StyleSheet} from 'react-native';
 
 /*
 <RNCamera
@@ -14,17 +15,49 @@ import { RNCamera } from 'react-native-camera';
           />
  */
 class BarcodeReader extends Component {
+  cameraReady() {
+    alert('Camera is ready dude.');
+  }
   render() {
     return (
-        <View style={{
-          flex: 1,
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}>
-          <Text>BARCODE</Text>
+        <View style={styles.container}>
+          <RNCamera
+              ref={ref => {
+                this.camera = ref;
+              }}
+              onCameraReady={() => this.cameraReady()}
+              style={styles.preview}
+              type={RNCamera.Constants.Type.front}
+              flashMode={RNCamera.Constants.FlashMode.off}
+              permissionDialogTitle={'Precisamos acessar a câmera'}
+              permissionDialogMessage={'Libere acesso à câmera para ler códigos de barra'}
+          />
         </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    backgroundColor: 'black'
+  },
+  preview: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    backgroundColor: 'aliceblue'
+  },
+  capture: {
+    flex: 0,
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    padding: 15,
+    paddingHorizontal: 20,
+    alignSelf: 'center',
+    margin: 20
+  }
+});
 
 export {BarcodeReader};
